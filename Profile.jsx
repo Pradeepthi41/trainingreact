6467182016d268299cc05360c2887ad660820e32
Pradeepthi41 <vpradeepthi41@gmail.com>
2025-02-13 13:06:18 +0530

    Add files via upload

---
+++ Profile.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import { Link, Outlet } from 'react-router-dom'
+
+const Profile = () => {
+  return (
+
+    <div style={{width:"100%",height:"100vh",background:"bisque"}}>Profile
+        <hr/>
+        <hr/>
+        <hr/>
+        <Link to='/profile/myaccount'>My Account</Link> <br/> <br/>
+        <Link to='/profile/mysettings'>My Setting</Link>
+
+        <Outlet/>
+    </div>
+  )
+}
+
+export default Profile
